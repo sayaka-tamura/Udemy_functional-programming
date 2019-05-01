2783ce543890a8812226238d1c5889721c50891a
sayaka-tamura <arccycle@gmail.com>
2019-05-01 18:44:58 +0900

    Add StreamIntermediateOperationsRunner class

---
+++ src/com/in28minutes/fanctionalprogramming/StreamIntermediateOperationsRunner.java
@@ -0,0 +1,28 @@
+/**
+ * @author : Sayaka Tamura
+ * May 1, 2019
+ * Udemy Learn Java Unit Testing with JUnit 5
+ * 
+ */
+package com.in28minutes.fanctionalprogramming;
+
+import java.util.List;
+
+public class StreamIntermediateOperationsRunner {
+
+	public static void main(String[] args) {
+		List<Integer> nums = List.of(3, 5, 3, 213, 45, 5, 7);
+		// Make a sorted list
+		// nums.stream().sorted().forEach(e -> System.out.println(e));
+
+		// Make a list that show distinct numbers
+		// nums.stream().distinct().forEach(e -> System.out.println(e));
+
+		// Make a sorted list that show distinct numbers
+		// nums.stream().sorted().distinct().forEach(e -> System.out.println(e));
+
+		// Make a sorted list from the square of each distinct numbers from <List> num
+		nums.stream().sorted().distinct().map(e -> e * e).forEach(e -> System.out.println(e));
+	}
+
+}
